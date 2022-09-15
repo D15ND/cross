a900e54411f827ba6fe674778000ee5537d5045e
ILYA <106765406+D15ND@users.noreply.github.com>
2022-09-15 18:21:35 +0300

    init

---
+++ js.js
@@ -0,0 +1,18 @@
+let day=prompt('what day of the week?', '');
+let hourWork=prompt('what time is it now?', '');
+let result;
+if (day=='mo' || day=='tu' || day=='we' || day=='th' || day=='fr' || day=='Mo' || day=='Tu' || day=='We' || day=='Th' || day=='Fr' || day=='Mon' || day=='Tue' || day=='Wed' || day=='Thu' || day=='Fri') {
+    if (hourWork<=10 || hourWork >=22) {
+        result=('office is closed - not work time');
+    }
+    else {
+        result=('office is the open - work time');
+    }
+}
+else {
+    result=('office is closed - weekend');
+}
+alert(result);
+console.log('result is:', result);
+console.log('input weekday is:', day);
+console.log('input time is:', hourWork);
